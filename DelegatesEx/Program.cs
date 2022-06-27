3b9d20a747315133695b30047cae13c4e0741700
Alexandr Rajcovschi <alexandr.rajcovschi@gmail.com>
2022-06-27 19:30:50 +0300

    Added basic delegates examples
    Simple usage of delegates and delegates declaration

---
+++ DelegatesEx/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace DelegatesEx
+{
+    class Program
+    {
+        public delegate float OperationDelegate(float num1, float num2);
+
+        public static float Add(float num1, float num2)
+        {
+            return num1 + num2;
+        } 
+        public static float Substract(float num1, float num2)
+        {
+            return num1 - num2;
+        } 
+        public static float Multiply (float num1, float num2)
+        {
+            return num1 * num2;
+        } 
+        public static float Divide(float num1, float num2)
+        {
+            return num1 / num2;
+        }
+        public static float ApplyOperation(float num1, float num2, OperationDelegate o)
+        {
+            float result = o(num1, num2);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ApplyOperation(1.2f, 1.5f, Substract)); 
+        }
+    }
+}
